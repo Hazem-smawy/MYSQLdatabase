4bf11ace7082203f0f1c0c9dc7b1eec38e65cf99
hazem-smawy <hazemsmawy@gmail.com>
2022-09-21 09:55:37 +0300

    Initial Commit

---
+++ MYSQLdatabase/MYSQLdatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYSQLdatabaseApp.swift
+//  MYSQLdatabase
+//
+//  Created by hazem smawy on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MYSQLdatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
